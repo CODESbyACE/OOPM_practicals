48408b6d3f5a0b749a6aedb4bad4a25811286bbc
CODESbyACE <codesbyace@gmail.com>
2023-10-15 12:19:59 +0530

    Array of Object code updated

--- arrOfObj.cpp
+++ arrOfObj.cpp
@@ -1,37 +1,45 @@
 #include<iostream>
+#include <ios>
+#include <limits>
 using namespace std;
 
 class Supermarket{
     private:
     char name[60];
-    int price;
+    float price;
     char category[20];
     public:
     void getData(){
         cout<<"Enter the Name of Product : ";
-        cin>>name;
+        
+        cin.getline(name,60);
         cout<<"Enter Price of Product : ";
         cin>>price;
         cout<<"Enter Category (Fruits/Vegetables/Dairy) : ";
-        cin>>category;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin.getline(category,20);
+        cout<< "\n";
     }
     void show_data(){
         cout << "Name: "<< name << endl
              << "Category: "<< category <<endl
              << "Price: "<< price << endl;
+        cout<< "\n";
     }
 };
 
 int main(){
     Supermarket s[6];
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 2; i++)
     {
+        cout<<i+1<<")";
         s[i].getData();
     }
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 2; i++)
     {
+        cout<<i+1<<")";
         s[i].show_data();
     }
 
